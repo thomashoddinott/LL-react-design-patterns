9c072f6e36148d184f933607ab1c89964f8179eb
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-10-27 11:47:47 +0100

    init React project and <SplitScreen> component

---
+++ layout-components/src/App.jsx
@@ -0,0 +1,22 @@
+import { SplitScreen } from './SplitScreen'
+import './App.css'
+
+const LeftHandComponent = () => {
+  return <h1 style={{backgroundColor: 'green'}}>Left</h1>
+}
+
+const RightHandComponent = () => {
+  return <p style={{backgroundColor: 'red'}}>Right</p>
+}
+
+function App() {
+
+  return (
+    <SplitScreen
+      left={LeftHandComponent}
+      right={RightHandComponent}
+    />
+  )
+}
+
+export default App
