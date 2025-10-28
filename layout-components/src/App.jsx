3ee4b8d831b495979ac6cb5684559e8b074e0c0a
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-10-28 11:23:34 +0100

    Combining components for complete demo

--- layout-components/src/App.jsx
+++ layout-components/src/App.jsx
@@ -3,63 +3,34 @@ import { LargePersonListItem } from "./LargePersonListItem";
 import { SmallPersonListItem } from "./SmallPersonListItem";
 import { Modal } from "./Modal";
 import { SplitScreen } from "./SplitScreen";
-
-const people = [
-  {
-    name: "John Doe",
-    age: 54,
-    hairColor: "brown",
-    hobbies: ["swimming", "bicycling", "video games"],
-  },
-  {
-    name: "Brenda Smith",
-    age: 33,
-    hairColor: "black",
-    hobbies: ["golf", "mathematics"],
-  },
-  {
-    name: "Jane Garcia",
-    age: 27,
-    hairColor: "blonde",
-    hobbies: ["biology", "medicine", "gymnastics"],
-  },
-];
-
-const products = [
-  {
-    name: "Flat-Screen TV",
-    price: "$300",
-    description: "Huge LCD screen, a great deal",
-    rating: 4.5,
-  },
-  {
-    name: "Basketball",
-    price: "$10",
-    description: "Just like the pros use",
-    rating: 3.8,
-  },
-  {
-    name: "Running Shoes",
-    price: "$120",
-    description: "State-of-the-art technology for optimum running",
-    rating: 4.2,
-  },
-];
+import { people } from "./data";
 
 function App() {
   return (
     <>
-      <Modal>
-        {/* <LargePersonListItem person={people[0]}/> */}
-        <SplitScreen>
+      <SplitScreen leftWeight={1} rightWeight={5}>
+        <>
           <h1>Left</h1>
           <RegularList
             items={people}
             resourceName="person"
-            itemComponent={SmallPersonListItem}
+            itemComponent={LargePersonListItem}
           />
-        </SplitScreen>
-      </Modal>
+        </>
+        <div style={{ paddingTop: 20 }}>
+          <Modal>
+            {/* <LargePersonListItem person={people[0]}/> */}
+            <SplitScreen>
+              <h1>Left</h1>
+              <RegularList
+                items={people}
+                resourceName="person"
+                itemComponent={SmallPersonListItem}
+              />
+            </SplitScreen>
+          </Modal>
+        </div>
+      </SplitScreen>
     </>
   );
 }
