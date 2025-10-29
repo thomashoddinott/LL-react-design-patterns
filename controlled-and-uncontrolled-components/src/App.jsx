ba9fb2d041203abe54cc00cd207ca5dedb06ea35
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-10-29 16:08:55 +0100

    A controlled modal component

--- controlled-and-uncontrolled-components/src/App.jsx
+++ controlled-and-uncontrolled-components/src/App.jsx
@@ -1,16 +1,26 @@
-import "./App.css";
+import { useState } from "react";
+
 import { UncontrolledForm } from "./UncontrolledForm";
 import { ControlledForm } from "./ControlledForm";
+import { ControlledModal } from "./ControlledModal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <>
-      <h3>Uncontrolled</h3>
-      <UncontrolledForm />
+      {/* <h3>Uncontrolled</h3> */}
+      {/* <UncontrolledForm /> */}
       {/* 👆 very inflexible */}
-      <h3>Controlled</h3>
-      <ControlledForm name_="thomas" age_={33} hairColor_="black" />
-      {/* 👆 more flexible */}
+
+      <ControlledModal
+        shouldShow={showModal}
+        onRequestClose={() => setShowModal(false)}
+      >
+        <h3>Controlled</h3>
+        <ControlledForm name_="thomas" age_={33} hairColor_="black" />
+        {/* 👆 more flexible */}
+      </ControlledModal>
+      <button onClick={() => setShowModal(true)}>Open Modal</button>
     </>
   );
 }
